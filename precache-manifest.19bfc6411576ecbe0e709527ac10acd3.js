cbd5ce19aa34627530546a46bfa1f25650964bbc
Eduardo <jeduardorjx@gmail.com>
2020-01-07 12:10:33 -0700

    Updates

---
+++ precache-manifest.19bfc6411576ecbe0e709527ac10acd3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c93d995ac5c27e491ebfddfe51c76f1",
+    "url": "/movie-tracker/index.html"
+  },
+  {
+    "revision": "6056307808b8fbbc5d97",
+    "url": "/movie-tracker/static/css/main.1d47212f.chunk.css"
+  },
+  {
+    "revision": "a92472ce1f67be71e521",
+    "url": "/movie-tracker/static/js/2.62302536.chunk.js"
+  },
+  {
+    "revision": "d0cb0a244b3563727e54b0f5d5982212",
+    "url": "/movie-tracker/static/js/2.62302536.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6056307808b8fbbc5d97",
+    "url": "/movie-tracker/static/js/main.ec012dc0.chunk.js"
+  },
+  {
+    "revision": "df58bab1462f43d84118",
+    "url": "/movie-tracker/static/js/runtime-main.fbc40ab1.js"
+  }
+]);
